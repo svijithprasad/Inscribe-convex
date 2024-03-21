d219caaff90a62efa2a2bee6ca1c837f174a54a5
Forloop <svijithprasad@gmail.com>
2024-03-21 12:25:17 +0530

    .

---
+++ dist/unsplashAPI.js
@@ -0,0 +1,7 @@
+const accessKey = "qMS3Kzw5ZwuZP7OTH1wYzNvKR3asXmK_aWEIUJybwo8";
+
+const coverImg = document.querySelector(".coverImg");
+
+coverImg.addEventListener("click", function () {
+    
+})
